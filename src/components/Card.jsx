85f4bdf6d377fb7d12bab0e37a0343d8e39ef151
Masterchinedum <chinedu02k@gmail.com>
2023-12-19 01:11:27 +0100

    code update

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,8 +5,7 @@ import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import { faLinkedin, faTwitter, faFacebook, faInstagram, faGithub } from "@fortawesome/free-brands-svg-icons";
 
 function Card(props) {
-  // destructure the props object
-  const { name, photo, email, github, linkedin, twitter, role, website, about, interests, facebook, instagram } = props;
+  const { name, photo, email, github, linkedin, twitter, facebook, instagram, role, website, about, interests } = props;
 
   return (
     <div className="card">
@@ -15,44 +14,56 @@ function Card(props) {
       </div>
       <div className="card-body">
         <h1 className="card-name">{name}</h1>
-        <p className="card-role"> {role} </p>
-        <p className="card-website"> {website} </p>
+        <p className="card-role">{role}</p>
+        <p className="card-website">{website}</p>
         <div className='btn-container'>
-          <a href= {email}>
-            <button className="btn btn-email">
-              <FontAwesomeIcon icon={faEnvelope} className="icon" />
-              Email
-            </button>
-          </a>
-          <a href= {linkedin}>
-            <button className="btn btn-linkedin">
-              <FontAwesomeIcon icon={faLinkedin} className="icon" />
-              Linkedin
-            </button>
-          </a>
+          {email && (
+            <a href={email}>
+              <button className="btn btn-email">
+                <FontAwesomeIcon icon={faEnvelope} className="icon" />
+                Email
+              </button>
+            </a>
+          )}
+          {linkedin && (
+            <a href={linkedin}>
+              <button className="btn btn-linkedin">
+                <FontAwesomeIcon icon={faLinkedin} className="icon" />
+                Linkedin
+              </button>
+            </a>
+          )}
         </div>
-        <h3 className='detail'>
-          About
-        </h3>
+        <h3 className='detail'>About</h3>
         <p className='card-text'>{about}</p>
-        <h3 className='detail'>
-          Interests
-        </h3>
-        <p className='card-text'>{interests}</p>
+        {interests && (
+          <>
+            <h3 className='detail'>Interests</h3>
+            <p className='card-text'>{interests}</p>
+          </>
+        )}
       </div>
       <div className="card-footer">
-        <a href={twitter} className="card-icon">
-        <FontAwesomeIcon icon={faTwitter} className="footericon" />
-        </a>
-        <a href={facebook} className="card-icon">
-        <FontAwesomeIcon icon={faFacebook} className= "footericon" />
-        </a>
-        <a href={instagram} className="card-icon">
-          <FontAwesomeIcon icon={faInstagram} className="footericon" />
-        </a>
-        <a href={github} className="card-icon">
-          <FontAwesomeIcon icon={faGithub} className="footericon" />
-        </a>
+        {twitter && (
+          <a href={twitter} className="card-icon">
+            <FontAwesomeIcon icon={faTwitter} className="footericon" />
+          </a>
+        )}
+        {facebook && (
+          <a href={facebook} className="card-icon">
+            <FontAwesomeIcon icon={faFacebook} className="footericon" />
+          </a>
+        )}
+        {instagram && (
+          <a href={instagram} className="card-icon">
+            <FontAwesomeIcon icon={faInstagram} className="footericon" />
+          </a>
+        )}
+        {github && (
+          <a href={github} className="card-icon">
+            <FontAwesomeIcon icon={faGithub} className="footericon" />
+          </a>
+        )}
       </div>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -50,29 +50,28 @@ root.render(
       name="James Hilthon"
       photo= {James}
       role= "Graphics design"
-      website= "https://jonathan.com"
+      website= "https://james.com"
       email="mailto:example@gmail.com"
-      github="https://github.com/johndoe"
-      linkedin="https://linkedin.com/in/johndoe"
-      twitter="https://twitter.com/johndoe"
-      facebook = "https://facebook.com/johndoe"
-      instagram = "https://instagram.com/johndoe"
+      github="https://github.com/jameshilthon"
+      linkedin="https://linkedin.com/in/jameshilthon"
+      twitter="https://twitter.com/jameshilthon"
+      facebook = "https://facebook.com/jameshilthon"
+      instagram = "https://instagram.com/jameshilthon"
       about = "I am a passionate graphics designer with an unwavering love for visual storytelling. My canvas is the digital realm, where pixels dance to create captivating designs. Whether it’s crafting sleek logos, illustrating whimsical characters, or designing user-friendly interfaces, I thrive on turning ideas into eye-catching realities."
 
     />
     <Card
       name="Mathew David"
       photo= {mathew}
-      role= "Frontend Developer"
-      website= "https://jonathan.com"
-      email="mailto:example@gmail.com"
-      github="https://github.com/johndoe"
-      linkedin="https://linkedin.com/in/johndoe"
-      twitter="https://twitter.com/johndoe"
-      facebook = "https://facebook.com/johndoe"
-      instagram = "https://instagram.com/johndoe"
-      about = "I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn."
-      interests = "Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic."
+      role= "Product Designer"
+      website= "https://Mathew.com"
+      github="https://github.com/mathewdavids"
+      linkedin="https://linkedin.com/in/mathewdavids"
+      twitter="https://twitter.com/mathewdavids"
+      facebook = "https://facebook.com/mathewdavids"
+      instagram = "https://instagram.com/mathewdavids"
+      about = "I believe that every curve, every gradient, and every negative space has a purpose. As a detail-oriented creator, I obsess over kerning, alignment, and the perfect shade of blue."
+      interests = "When I’m not pushing pixels, you’ll find me exploring new typefaces, experimenting with vector art, and staying up-to-date with design trends. I’m a firm believer in the power of minimalism, but I also appreciate the occasional flourish that adds that extra spark."
     />
     </div>
     <Footer />
